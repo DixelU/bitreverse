fba5489de1890ed140d461540df0ba4cf98c747e
Alexander Verevkin <89090823215@yandex.ru>
2023-08-03 00:20:26 +0500

    md5 implementation

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+﻿#include <iostream>
 
 #include "bitreverse.h"
 #include "counted_ptr.h"
@@ -69,6 +69,22 @@ void inplace_calculation_test()
     std::cout << "Bit depth: " << c.__max_depth() << std::endl;
 }
 
+
+template<typename F>
+void hashing_test(size_t string_size, F f)
+{
+    std::cout << std::endl << "Hashing test. String size: " << string_size << std::endl;
+
+    std::vector<dixelu::bitreverse::itu8> unknown_string(string_size, dixelu::bitreverse::unknown);
+
+    for (auto& ch : unknown_string)
+        ch &= 0x7F;//ascii symbols
+
+    auto result = f(unknown_string);
+
+    std::cout << result.__to_string() << " " << result.__max_depth() << std::endl;
+}
+
 dixelu::bitreverse::itu32 crc32(std::vector<dixelu::bitreverse::itu8> message)
 {
     dixelu::bitreverse::itu32 byte, mask;
@@ -129,19 +145,6 @@ dixelu::bitreverse::itu32 crc32(std::vector<dixelu::bitreverse::itu8> message)
     return ~crc;
 }
 
-void crc32_test(size_t count = 2)
-{
-    std::cout << std::endl << "crc32_test. string size: " << count << std::endl;
-
-    std::vector<dixelu::bitreverse::itu8> unknown_string(count, dixelu::bitreverse::unknown);
-    
-    for (auto& ch : unknown_string)
-        ch &= 0x7F;//ascii symbols
-
-    dixelu::bitreverse::itu32 result = crc32(unknown_string);
-
-    std::cout << result.__to_string() << " " << result.__max_depth() << std::endl;
-}
 
 void add_substract_test()
 {
@@ -160,13 +163,143 @@ void add_substract_test()
     std::cout << nota.__to_string() << std::endl;
 }
 
+dixelu::bitreverse::int_tracker<128> md5(std::vector<dixelu::bitreverse::itu8> message)
+{
+    using itu128 = dixelu::bitreverse::int_tracker<128>;
+    using itu32 = dixelu::bitreverse::itu32;
+    using itu8 = dixelu::bitreverse::itu8;
+
+    constexpr uint8_t s[64] = {
+        7, 12, 17, 22,  7, 12, 17, 22,  7, 12, 17, 22,  7, 12, 17, 22,
+        5,  9, 14, 20,  5,  9, 14, 20,  5,  9, 14, 20,  5,  9, 14, 20,
+        4, 11, 16, 23,  4, 11, 16, 23,  4, 11, 16, 23,  4, 11, 16, 23,
+        6, 10, 15, 21,  6, 10, 15, 21,  6, 10, 15, 21,  6, 10, 15, 21
+    };
+
+    constexpr uint32_t K[64] = {
+        0xd76aa478, 0xe8c7b756, 0x242070db, 0xc1bdceee,
+        0xf57c0faf, 0x4787c62a, 0xa8304613, 0xfd469501,
+        0x698098d8, 0x8b44f7af, 0xffff5bb1, 0x895cd7be,
+        0x6b901122, 0xfd987193, 0xa679438e, 0x49b40821,
+        0xf61e2562, 0xc040b340, 0x265e5a51, 0xe9b6c7aa,
+        0xd62f105d, 0x02441453, 0xd8a1e681, 0xe7d3fbc8,
+        0x21e1cde6, 0xc33707d6, 0xf4d50d87, 0x455a14ed,
+        0xa9e3e905, 0xfcefa3f8, 0x676f02d9, 0x8d2a4c8a,
+        0xfffa3942, 0x8771f681, 0x6d9d6122, 0xfde5380c,
+        0xa4beea44, 0x4bdecfa9, 0xf6bb4b60, 0xbebfbc70,
+        0x289b7ec6, 0xeaa127fa, 0xd4ef3085, 0x04881d05,
+        0xd9d4d039, 0xe6db99e5, 0x1fa27cf8, 0xc4ac5665,
+        0xf4292244, 0x432aff97, 0xab9423a7, 0xfc93a039,
+        0x655b59c3, 0x8f0ccc92, 0xffeff47d, 0x85845dd1,
+        0x6fa87e4f, 0xfe2ce6e0, 0xa3014314, 0x4e0811a1,
+        0xf7537e82, 0xbd3af235, 0x2ad7d2bb, 0xeb86d391
+    };
+
+    itu32 a0 = 0x67452301;
+    itu32 b0 = 0xefcdab89;
+    itu32 c0 = 0x98badcfe;
+    itu32 d0 = 0x10325476;
+    
+    uint64_t message_size_bits = message.size() * 8;
+    message.push_back(0x80);
+    while (message.size() % 64 != 56)
+        message.push_back(0);
+
+    for(size_t i = 0; i < 8; ++i)
+    {
+        auto value = ((uint8_t*)&message_size_bits)[i];
+        message.push_back(value);
+    }
+
+    auto combine = [](const itu8& a, const itu8& b, const itu8& c, const itu8& d)
+    {
+        return (itu32(a) << 24) | (itu32(b) << 16) | (itu32(c) << 8) | itu32(d);
+    };
+
+    auto leftrotate = [](itu32 value, size_t d) -> itu32
+    {
+        return (value << d) | (value >> (32 - d));
+    };
+
+    auto reverse_endianness = [](itu32 val)
+    {
+        return
+            (((val) & 0xFF) << 24) |
+            (((val >> 8) & 0xFF) << 16) |
+            (((val >> 16) & 0xFF) << 8) |
+            (((val >> 24) & 0xFF));
+    };
+
+    for (size_t q = 0; q < message.size(); q += 64)
+    {
+        itu32 M[16];
+        for (size_t j = 0; j < 64; j += 4)
+        {
+            auto& M_elem = M[j >> 2];
+            M_elem = combine(message[j + 3], message[j + 2], message[j + 1], message[j + 0]);
+            std::cout << M_elem.__to_string() << std::endl;
+        }
+
+        itu32 A = a0;
+        itu32 B = b0;
+        itu32 C = c0;
+        itu32 D = d0;
+
+        for (size_t i = 0; i < 64; ++i)
+        {
+            itu32 F;
+            size_t g;
+            if (i <= 15)
+            {
+                F = (B & C) | ((~B) & D);
+                g = i;
+            }
+            else if (i <= 31)
+            {
+                F = (D & B) | ((~D) & C);
+                g = (5 * i + 1) % 16;
+            }
+            else if (i <= 47)
+            {
+                F = B ^ C ^ D;
+                g = (3 * i + 5) % 16;
+            }
+            else if (i <= 63)
+            {
+                F = C ^ (B | (~D));
+                g = (7 * i) % 16;
+            }
+
+            F = F + A + K[i] + M[g];
+            A = D;
+            D = C;
+            C = B;
+            B = B + leftrotate(F, s[i]);
+        }
+
+        a0 += A;
+        b0 += B;
+        c0 += C;
+        d0 += D;
+    }
+
+    return 
+        (itu128(reverse_endianness(a0)) << (32 + 64)) |
+        (itu128(reverse_endianness(b0)) << 64) |
+        (itu128(reverse_endianness(c0)) << 32) |
+        itu128(reverse_endianness(d0));
+}
+
 int main()
 {
     counted_simple_test();
     counted_test_with_enable_counted_from_this();
     inplace_calculation_test();
     add_substract_test();
-    crc32_test(16);
+    //hashing_test(16, crc32);
+    //hashing_test(16, md5);
+
+    std::cout << md5({'m', 'd', '5'}).__to_string() << std::endl;
 
     return 0;
 }
