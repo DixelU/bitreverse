fe431f26758eec4ac6e52f16a09b38507fd9fd31
Alexander Verevkin <89090823215@yandex.ru>
2025-09-11 22:18:13 +0500

    yeah... it is too slow, obviously

--- main.cpp
+++ main.cpp
@@ -44,6 +44,69 @@ void bitwise_reversal_test()
 	}
 }
 
+template<template<size_t> typename int_tracker>
+int_tracker<32> crc32(std::vector<int_tracker<8>> message)
+{
+	int_tracker<32> byte, mask;
+
+	int_tracker<32> crc = 0xFFFFFFFF;
+	const int_tracker<32> mask_const = 0xEDB88320;
+
+	for (auto& ch : message)
+	{
+		byte = int_tracker<32>(ch);
+		crc = crc ^ byte;
+
+		for (int j = 7; j >= 0; j--)
+		{
+			mask = -(crc & 1);
+			crc = (crc >> 1) ^ (mask_const & mask);
+		}
+	}
+	return ~crc;
+}
+
+void real_crc32_reversal()
+{
+	using dixelu::bitreverse::unknown;
+
+	std::vector<dixelu::bitreverse::itu8> hashed_string = { unknown, unknown, unknown, unknown, unknown, unknown, unknown };
+	std::array<dixelu::bitreverse::bit_tracker, 32> target_hash
+	{ 0,0,0,0,0,1,0,0,1,1,0,0,1,1,1,1,1,1,1,0,1,1,0,1,0,0,0,0,0,0,0,1 };
+	dixelu::bitreverse::int_tracker<32> result(std::move(target_hash));
+
+	dixelu::bitreverse::bit_tracker hashed_string_is_not_ascii;
+	for (auto& itu8 : hashed_string)
+	{
+		hashed_string_is_not_ascii |= dixelu::bitreverse::bit_tracker(itu8 & 0x80); // is beyond ascii
+		hashed_string_is_not_ascii |=
+			dixelu::bitreverse::bit_tracker((itu8 - 31) & 0x80) &  // is control symbol
+			dixelu::bitreverse::bit_tracker(itu8); // and not zero
+	}
+
+	auto crc32_result = crc32(hashed_string);
+	auto crc32_result_differs = dixelu::bitreverse::bit_tracker(crc32_result ^ result);
+	auto is_false = crc32_result_differs | hashed_string_is_not_ascii;
+	dixelu::bitreverse::bit_tracker _true(true);
+
+	std::cout << "Real CRC32 reversal test" << std::endl;
+	auto reversal_result = dixelu::bitreverse::assert_equality(is_false, _true);
+
+	size_t counter = 0;
+	std::cout << "Size: " << reversal_result.size() << std::endl;
+	for (auto& solution : reversal_result)
+	{
+		++counter; 
+		std::cout << "=== SOLUTION " << counter << " ===" << std::endl;
+		for (auto single_char : hashed_string)
+		{
+			dixelu::bitreverse::assign_assert_result<8>(single_char, solution.assignments);
+			std::cout << single_char.__to_string();
+		}
+		std::cout << std::endl;
+	}
+}
+
 int main()
 {
 	// dixelu::bitreverse::itu8 x = 0b01101010;
